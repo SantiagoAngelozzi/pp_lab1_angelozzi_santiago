5f29763b999d3eecba11beb5ad888b86907135d0
SantiagoAngelozzi <santiagoangelozzi@gmail.com>
2023-05-26 15:42:11 -0300

    punto 2 y 3 terminados

--- biblioteca.py
+++ biblioteca.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 #0) funciones reutilizables o complementarias.
 def clear_console() -> None:
     """
@@ -25,6 +26,31 @@ def leer_archivo_json(ruta: str) -> list:
         lista_jugadores = contenido["jugadores"]
     return lista_jugadores
 
+def guardar_archivo_csv(nombre_archivo: str, contenido: str) -> bool:
+    """
+    Esta función guarda el contenido de una cadena en un archivo con el nombre de archivo dado y
+    devuelve un valor booleano que indica si la operación fue exitosa o no.
+
+    Parametros: 
+        -nombre_archivo: Una cadena que representa el nombre del archivo que se va a crear o
+        sobrescribir
+
+        -contenido: El contenido que se escribirá en el archivo. Debería ser una cadena
+
+    :retorno: 
+        -un valor booleano, ya sea True o False, según si el archivo se creó correctamente o no.
+    """
+
+    with open(nombre_archivo, 'w+') as archivo:
+        resultado = None
+        resultado = archivo.write(contenido)
+    if resultado:
+        print("Se creó el archivo: {0}".format(nombre_archivo))
+        return True
+
+    print("Error al crear el archivo: {0}".format(nombre_archivo))
+    return False
+
 def imprimir_dato(dato : str):
     '''
     recive un dato tipo str
@@ -33,49 +59,24 @@ def imprimir_dato(dato : str):
     '''
     print(dato)
 
-def validar_entero(num) -> bool:
-    '''
-    recive un numero
-    verifica que se un numero
-    devuelve un bool
-    '''
-    return num.isdigit()
-
 def imprimir_menu():
     '''
     imprime el menu
     '''
-    imprimir_dato("1) Mostrar la lista de todos los jugadores del Dream Team\n\
-                   2) \n\
-                   3) \n\
-                   4) \n\
-                   5) \n\
-                   6) \n\
-                   7) \n\
-                   8) \n\
-                   9) \n\
-                   10) \n\
-                   11) \n\
-                   12) \n\
-                   13) \n\
-                   14) \n\
-                   15) \n\
-                   16) \n\
-                   17) \n\
-                   18) \n\
-                   19) \n\
-                   20)  ")
+    imprimir_dato("1) Mostrar la lista de todos los jugadores del Dream Team\n2) elije un jugador para mostrar sus estadisticas\n3) elije un jugador para mostrar sus estadisticas y descargarlo en csv \n4) \n5) \n6) \n7) \n8) \n9) \n10) \n11) \n12) \n13) \n14) \n15) \n16) \n17) \n18) \n19) \n20)  ")
 
 def dream_team_menu_principal():
     '''
     verifica que sea un numero con la funcion previamente creada
     retorna un numero entero
     '''
-    opcion = input("Ingrese una opción: ")
-    while not validar_entero(opcion):
-        print("Error: debe ingresar un número entero")
-        opcion = input("Ingrese una opción: ")
-    return int(opcion)
+    opcion = input("Seleccione opción: ")
+    if re.match(r"^(?:[1-9]|1\d|20)$",opcion):
+        opcion = int(opcion)
+        return opcion
+    else:
+        print("Opción inválida. Inténtelo nuevamente")
+        return "-1"
 
 def dream_team_app(jugadores: list):
     '''
@@ -90,9 +91,9 @@ def dream_team_app(jugadores: list):
             case 1:
                 mostrar_jugadores(jugadores)
             case 2:
-                pass
+                seleccionar_jugador(jugadores)
             case 3:
-                pass
+                guardar_estadisticas_csv(jugadores)
             case 4:
                 pass
             case 5:
@@ -134,6 +135,7 @@ def dream_team_app(jugadores: list):
 # 1) Mostrar la lista de todos los jugadores del Dream Team. Con el formato:
 # Nombre Jugador - Posición. Ejemplo:
 # Michael Jordan - Escolta 
+
 def mostrar_jugadores(jugadores: list):
     """""
     recive como parametro una lista de jugadores
@@ -143,3 +145,80 @@ def mostrar_jugadores(jugadores: list):
     if jugadores:
         for jugador in jugadores:
             imprimir_dato(f"{jugador['nombre']} - {jugador['posicion']}")
+
+#  2) Permitir al usuario seleccionar un jugador por su índice y mostrar sus estadísticas completas,
+#  incluyendo temporadas jugadas, puntos totales, promedio de puntos por partido, rebotes totales,
+#  promedio de rebotes por partido, asistencias totales, promedio de asistencias por partido, robos totales,
+#  bloqueos totales, porcentaje de tiros de campo, porcentaje de tiros libres y porcentaje de tiros triples.
+
+def mostrar_estadisticas_jugador(jugador) -> str:
+    """""
+    recive un jugador como parametro
+    imprime nombre, pocicion y todas las estadisticas
+    retorna la lista de estadisticas del jugador
+    """""
+    dato = f"{jugador['nombre']} - {jugador['posicion']}\
+    \nTemporadas jugadas: {jugador['estadisticas']['temporadas']}\
+    \nPuntos totales: {jugador['estadisticas']['puntos_totales']}\
+    \nPromedio de puntos por partido: {jugador['estadisticas']['promedio_puntos_por_partido']}\
+    \nRebotes totales: {jugador['estadisticas']['rebotes_totales']}\
+    \nPromedio de rebotes por partido: {jugador['estadisticas']['promedio_rebotes_por_partido']}\
+    \nAsistencias totales: {jugador['estadisticas']['asistencias_totales']}\
+    \nPromedio de asistencias por partido: {jugador['estadisticas']['promedio_asistencias_por_partido']}\
+    \nRobos totales: {jugador['estadisticas']['robos_totales']}\
+    \nBloqueos totales: {jugador['estadisticas']['bloqueos_totales']}\
+    \nPorcentaje de tiros de campo: {jugador['estadisticas']['porcentaje_tiros_de_campo']}\
+    \nPorcentaje de tiros libres: {jugador['estadisticas']['porcentaje_tiros_libres']}\
+    \nPorcentaje de tiros triples: {jugador['estadisticas']['porcentaje_tiros_triples']}"
+    imprimir_dato(dato)
+    return dato
+    
+
+def seleccionar_jugador(jugadores: list):
+    """""
+    recive como parametro una lista de jugadores 
+    recorre la lista imprime los jugadores ordenados por indice y da a elejir uno
+    """""
+    indice = 0
+    for jugador in jugadores:
+        indice += 1
+        print(f"{indice}) {jugador['nombre']}")
+    seleccion = int(input("Selecciona un jugador por su índice: "))
+    seleccion -= 1
+    if seleccion >= 0 and seleccion < len(jugadores):
+        jugador = jugadores[seleccion]
+        dato = mostrar_estadisticas_jugador(jugador)
+    else:
+        print("Índice de jugador inválido.")
+    return dato
+    
+
+# 3) Después de mostrar las estadísticas de un jugador seleccionado por el usuario,
+# permite al usuario guardar las estadísticas de ese jugador en un archivo CSV.
+# El archivo CSV debe contener los siguientes campos: nombre, posición, temporadas, puntos totales,
+# promedio de puntos por partido, rebotes totales, promedio de rebotes por partido, asistencias totales,
+# promedio de asistencias por partido, robos totales, bloqueos totales, porcentaje de tiros de campo,
+# porcentaje de tiros libres y porcentaje de tiros triples.
+
+def guardar_estadisticas_csv(jugadores):
+    contenido = seleccionar_jugador(jugadores)
+    archivo = "estadisticas_jugador.csv"
+    guardar_archivo_csv(archivo, contenido)
+
+
+
+    
+
+
+
+
+
+
+
+    
+    
+    
+
+
+    
+
